edd85b05ff3f53768253b1a5fb496e89c3c26dd6
666chen666 <669242512@qq.com>
2017-05-02 11:47:05 +0800

    112

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -40,9 +40,9 @@ return array(
     /* 数据库配置 */
     'DB_TYPE'   => 'mysqli', // 数据库类型
     'DB_HOST'   => '127.0.0.1', // 服务器地址
-    'DB_NAME'   => 'onethink', // 数据库名
-    'DB_USER'   => 'root', // 用户名
-    'DB_PWD'    => 'root',  // 密码
+    'DB_NAME'   => 'onethink1', // 数据库名
+    'DB_USER'   => 'cpd123', // 用户名
+    'DB_PWD'    => '123321',  // 密码
     'DB_PORT'   => '3306', // 端口
     'DB_PREFIX' => 'onethink_', // 数据库表前缀
 
