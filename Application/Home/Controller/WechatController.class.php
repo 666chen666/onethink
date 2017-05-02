cf25c9dc841f16abe4d4ec3e65516c05c4088fb9
666chen666 <669242512@qq.com>
2017-05-02 14:07:34 +0800

    22222

--- Application/Home/Controller/WechatController.class.php
+++ Application/Home/Controller/WechatController.class.php
@@ -20,12 +20,69 @@ class WechatController extends HomeController
         // 将响应输出
         $response->send();
     }
-    public function actionUser(){
+    public function getmenus(){
+        $app = new Application(C('wechat'));
+        $menu = $app->menu;
+        $menus = $menu->all();
+        var_dump($menus);
+    }
+
+    public function setmenus()
+    {
+        $app = new Application(C('wechat'));
+        $menu = $app->menu;
+        $buttons = [
+            [
+                "type" => "click",
+                "name" => "热销商品",
+                "key"  => "V1001_HOT"
+            ],
+            [
+                "name"       => "个人中心",
+                "sub_button" => [
+                    [
+                        "type" => "view",
+                        "name" => "物业管理",
+                        "url"  => U('home/index/index','','',true)
+                    ],
+                ],
+            ],
+        ];
+        $r = $menu->add($buttons);
+        var_dump($r);
+    }
+
+    //用户帐号信息
+    public function user(){
+        //如果session里面没有openid 就获取openid
+        if(empty(session('openid'))){
             $app = new Application(C('wechat'));
-            $response = $app->oauth->redirect();
-            //将当前路由保存到session，便于授权回调地址跳回当前页面
-            \Yii::$app->session->setFlash('back','wechat/user');
+            $response = $app->oauth->scopes(['snsapi_base'])
+                ->redirect();
             $response->send();
-        var_dump(\Yii::$app->session->get('openid'));
+        }
+        //从session中获取openid
+        $openid = session('openid');
+        //根据openid查询数据库中是否有数据
+        $member = M('member')->find($openid);
+        //没有数据 跳转到绑定页面
+        if($member == null){
+            $this->redirect(['wechat/bang']);
+        }
+        //有数据 获取数据
+        $this->assign('member',$member);
+        $this->display('user');
+    }
+
+    //授权回调
+    public function callback(){
+        $app = new Application(C('wechat'));
+        $user = $app->oauth->user();
+        //将用户的openid保存到session
+        session('openid',$user->id);
+    }
+
+    public function bang(){
+        var_dump(session('openid'));
     }
 }
